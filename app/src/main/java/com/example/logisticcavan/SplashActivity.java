e43328f386239c1139f04da104fc077b41065c7a
muhammednashat <mnashat.dev@gmail.com>
2024-10-06 23:24:53 +0300

    create auth flow

---
+++ app/src/main/java/com/example/logisticcavan/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.logisticcavan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.logisticcavan.auth.presentation.AuthActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        new Handler()
+                .postDelayed(this::isLogged
+                        , 2000L);
+
+    }
+
+
+    public void isLogged() {
+
+        Intent intent = new Intent(SplashActivity.this, AuthActivity.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/logisticcavan/auth/presentation/AuthViewModel.java
@@ -0,0 +1,8 @@
+package com.example.logisticcavan.auth.presentation;
+
+import androidx.lifecycle.ViewModel;
+
+public class AuthViewModel  extends ViewModel {
+
+
+}
