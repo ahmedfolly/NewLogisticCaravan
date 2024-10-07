3f5148e2d03bd5974c051d118dbb1a8d908a6a92
muhammednashat <mnashat.dev@gmail.com>
2024-10-08 00:11:51 +0300

    completed auth flow

--- app/src/main/java/com/example/logisticcavan/ChooseFragment.java
+++ app/src/main/java/com/example/logisticcavan/ChooseFragment.java
@@ -1,4 +1,4 @@
-package com.example.logisticcavan.auth.presentation;
+package com.example.logisticcavan;
 
 import android.os.Bundle;
 
@@ -13,22 +13,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.logisticcavan.R;
+import com.example.logisticcavan.databinding.FragmentChooseBinding;
 import com.example.logisticcavan.databinding.FragmentLoginBinding;
-import com.example.logisticcavan.databinding.FragmentMainAuthBinding;
 
 
-public class LoginFragment extends Fragment {
+public class ChooseFragment extends Fragment {
 
-    private FragmentLoginBinding binding;
+    private FragmentChooseBinding binding;
     private NavController navController;
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_choose, container, false);
         return binding.getRoot();
 
     }
@@ -36,19 +34,18 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         navController = Navigation.findNavController(view);
         setUpClickListener();
     }
 
     private void setUpClickListener() {
 
-        binding.forgetPass.setOnClickListener(view -> {
-            navController.navigate(R.id.action_loginFragment_to_restorPasswordFragment);
+        binding.customer.setOnClickListener(view -> {
+            navController.navigate(R.id.action_chooseFragment_to_signUpFragment);
         });
 
-        binding.signUp.setOnClickListener(view -> {
-            navController.navigate(R.id.action_loginFragment_to_chooseFragment);
+        binding.courier.setOnClickListener(view -> {
+            navController.navigate(R.id.action_chooseFragment_to_signUpFragment);
         });
     }
 
--- app/src/main/java/com/example/logisticcavan/auth/presentation/MainAuthFragment.java
+++ app/src/main/java/com/example/logisticcavan/auth/presentation/MainAuthFragment.java
@@ -1,27 +1,58 @@
 package com.example.logisticcavan.auth.presentation;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import com.example.logisticcavan.R;
+import com.example.logisticcavan.databinding.FragmentMainAuthBinding;
+
+import java.util.Objects;
 
 public class MainAuthFragment extends Fragment {
 
 
+    private FragmentMainAuthBinding binding;
+    private NavController navController;
 
-    public MainAuthFragment() {
+    public MainAuthFragment(){
+        
     }
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_auth, container, false);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main_auth, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        setUpClickListener();
+    }
+
+    private void setUpClickListener() {
+
+        binding.signIn.setOnClickListener(view -> {
+            navController.navigate(R.id.action_mainAuthFragment_to_loginFragment);
+        });
+
+        binding.signUp.setOnClickListener(view -> {
+            navController.navigate(R.id.action_mainAuthFragment_to_chooseFragment);
+        });
     }
 }
