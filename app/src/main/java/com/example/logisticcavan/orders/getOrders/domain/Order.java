265fc5e08d53b9a0fbb7d08f3c4bf69ec5e93710
Ahmed Folly <ahmedfolly124@gmail.com>
2024-10-16 16:49:44 +0300

    Order model

---
+++ app/src/main/java/com/example/logisticcavan/orders/getOrders/domain/Order.java
@@ -0,0 +1,98 @@
+package com.example.logisticcavan.orders.getOrders.domain;
+
+public class Order {
+
+    private String dateCreated = "";
+    private String from = "";
+    private String location = "";
+    private String payment = "";
+    private double price = 0.0;
+    private String productId = "";
+    private String pending = "";
+    private String to = "";
+    private int totalAmount = 0;
+    private double totalCost = 0.0;
+
+    public Order() {
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getPending() {
+        return pending;
+    }
+
+    public void setPending(String pending) {
+        this.pending = pending;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(int totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+}
