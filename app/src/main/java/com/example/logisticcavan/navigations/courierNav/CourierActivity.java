cd2126a490df892083eed7e00710207a1caf2776
muhammednashat <mnashat.dev@gmail.com>
2024-10-24 15:57:24 +0300

    implement setting flow

--- app/src/main/java/com/example/logisticcavan/navigations/courierNav/CourierActivity.java
+++ app/src/main/java/com/example/logisticcavan/navigations/courierNav/CourierActivity.java
@@ -4,9 +4,14 @@ package com.example.logisticcavan.navigations.courierNav;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import com.example.logisticcavan.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import androidx.navigation.ui.NavigationUI;
 
 @AndroidEntryPoint
 public class CourierActivity extends AppCompatActivity {
@@ -18,6 +23,12 @@ public class CourierActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_courier);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavController navController = Navigation.findNavController(this, R.id.host_fragment_courier);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
     }
+
 
 }
