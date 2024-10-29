dffcdaf6d049fdee17f7b637aa002078d221a89d
muhammednashat <mnashat.dev@gmail.com>
2024-10-29 15:56:06 +0300

    implement get notification remotely

--- app/src/main/java/com/example/logisticcavan/orders/getOrders/courier/presentaion/OrderDetailFragment.java
+++ app/src/main/java/com/example/logisticcavan/orders/getOrders/courier/presentaion/OrderDetailFragment.java
@@ -9,11 +9,14 @@ import com.example.logisticcavan.common.base.BaseFragment;
 import com.example.logisticcavan.databinding.FragmentOrderDetailBinding;
 import com.example.logisticcavan.orders.getOrders.domain.Order;
 
+import java.util.List;
+import java.util.Map;
+
 
 public class OrderDetailFragment extends BaseFragment {
 
     private FragmentOrderDetailBinding binding;
-
+    String orderId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +29,47 @@ public class OrderDetailFragment extends BaseFragment {
     }
 
     private void setUpUiData(Order order) {
+        Map<String,String> customerMap = order.getCustomer();
+        String customerName = customerMap.get("name");
+
+        List<Map<String, Object>> cartItemsMap = order.getCartItems();
+        Map<String,Object> generaDetails = order.getGeneralDetails();
+
+        Map<String, String> location = order.getLocation();
+        Map<String, String> deliveryTime = order.getDeliveryTime();
+        Map<String, String> restaurant = order.getRestaurant();
+        String restaurantName = restaurant.get("name");
+        String orderId = generaDetails.get("orderId").toString();
+        String orderStatus = generaDetails.get("status").toString();
+
+        binding.address.setText(location.get("beach")+", Villa"+location.get("villaNum"));
+        binding.date.setText(deliveryTime.get("date"));
+        binding.time.setText(deliveryTime.get("time"));
+        binding.deliveryId.setText("#"+orderId.substring(0,5));
+
+
+        setTextButton(orderStatus);
+        if(orderStatus.equals("pending")){
+
 
     }
 }
+
+    private void setTextButton(String orderStatus) {
+     String text = "";
+      switch (orderStatus){
+          case "Pending":
+             text = "Update Status";
+      break;
+      case "Delivered":{
+          text = "Delivered";
+          binding.startDelivery.setEnabled(false);
+          break;
+      }
+
+          default:
+           text ="Start delivery";
+      }
+    binding.startDelivery.setText(text);
+    }
+    }
