1b92630b0c6571f9a77b5f30b83487f75a2be00d
Ahmed Folly <ahmedfolly124@gmail.com>
2024-10-31 13:37:42 +0300

    asdf

--- app/src/main/java/com/example/logisticcavan/PlaceOrderFragment.java
+++ app/src/main/java/com/example/logisticcavan/PlaceOrderFragment.java
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
